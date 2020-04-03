09f62077a2a34ae82f595fe264054b690404a2e6
taoqf <tao_qiufeng@126.com>
2020-04-03 10:46:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 
-export default function awx18(param1: string, param2: string) {
+export default function preview_image(urls: string[], current?: string) {
+	return new Promise<boolean>((resolve, reject) => {
+		wx.previewImage({
+			current, 		// 当前显示图片的http链接
+			urls, 			// 需要预览的图片http链接列表
+			success() {
+				resolve(true);
+			},
+			fail(res) {
+				reject(res);
+			}
+		});
+	});
 }
